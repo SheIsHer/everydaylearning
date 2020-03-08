3e18c9482352c5ce10283cc608f499442c96cbed
lty <1353564917@qq.com>
2020-03-09 02:55:33 +0800

    完成了分类参数的开发

--- 2020.3/2020.3.5/project/vue_shop/src/main.js
+++ 2020.3/2020.3.5/project/vue_shop/src/main.js
@@ -6,6 +6,8 @@ import './plugins/element.js'
 import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
+
 Vue.prototype.$http = axios
 axios.defaults.baseURL = 'http://119.23.53.78:8888/'
 axios.interceptors.request.use(config => {
@@ -14,7 +16,7 @@ axios.interceptors.request.use(config => {
   return config;
 })
 Vue.config.productionTip = false
-
+Vue.component('tree-table', TreeTable)
 new Vue({
   router,
   render: h => h(App)
